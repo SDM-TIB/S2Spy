e770dd13fd8c94ad51aaed26f90352ddd1ca55af
Monica Figuera <monicafigueray@gmail.com>
2019-08-13 19:44:19 +0200

    clean up: data graph filename is the same as shapes graph filename

--- examples/smoker.py
+++ examples/smoker.py
@@ -4,41 +4,36 @@ from SHACLtoConstructQuery import get_construct_query
 from SPARQLWrapper import SPARQLWrapper, TURTLE
 from rdflib import Graph
 from pyshacl import validate
+from utils import lastStringURL
 
 def main():
-    retrieveFromEndpoint = False
-
-    if retrieveFromEndpoint:
-        sparql = SPARQLWrapper("http://node2.research.tib.eu:18971/sparql")
+    sparql = SPARQLWrapper("http://node2.research.tib.eu:18971/sparql")
 
     sg = "./shapes/smoker.ttl"
     sg = path.abspath(sg)
 
-    data_ttl_file = "./data/smoker_ex.ttl"
-    data_ttl_file = path.abspath(data_ttl_file)
-
     construct_query = get_construct_query(sg)
     print("Construct query:\n", construct_query)
 
-    if retrieveFromEndpoint:
-        sparql.setQuery(construct_query)
+    sparql.setQuery(construct_query)
 
-        ######################################
+    ######################################
 
-        print('*** Creating file in Turtle Format ***')
-        sparql.setReturnFormat(TURTLE)
-        results = sparql.query().convert()
-        g = Graph()
-        g.parse(data=results, format="turtle")
-        g.serialize(format='turtle')
+    print('*** Creating file in Turtle Format ***')
+    sparql.setReturnFormat(TURTLE)
+    results = sparql.query().convert()
+    g = Graph()
+    g.parse(data=results, format="turtle")
+    g.serialize(format='turtle')
 
-        file_name = "data/output.ttl"
-        file = open(file_name, "wb")
-        g.serialize(destination=file, format="turtle")
-        file.flush()
-        file.close()
-        #print(g)
-        data_ttl_file = path.abspath(file_name)
+    filename = lastStringURL(sg)[1]
+    file_path = "data/" + filename
+    file = open(file_path, "wb")
+    g.serialize(destination=file, format="turtle")
+    file.flush()
+    file.close()
+
+    data_ttl_file = path.abspath(file_path)
 
     conforms, v_graph, v_text = validate(data_ttl_file, shacl_graph=sg, inference='rdfs',
                                          serialize_report_graph=True)
