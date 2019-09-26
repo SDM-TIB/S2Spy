b2c816be5bd5c0f76349b8af51cd9f9f2c2338fb
Monica Figuera <monicafigueray@gmail.com>
2019-09-26 19:42:54 +0200

    clean up: running all tests against hop2, added argument to decide whether to use the wrapper or not

---
+++ examples/all_hop2.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import sys
+from os import path
+from pyshacl import validate
+from pyshacl import get_construct_query
+from SPARQLWrapper import SPARQLWrapper, TURTLE
+from rdflib import Graph
+
+from utils import lastStringURL
+import time
+
+
+def shapes_graph_to_test():
+    constraints = []
+    constraints.append("./shapes/smoker.ttl")
+    constraints.append("./shapes/patient_stage.ttl")
+    constraints.append("./shapes/biomarker.ttl")
+
+    return constraints
+
+def get_reduced_data_from_endpoint(sg):
+    sparql = SPARQLWrapper("http://node3.research.tib.eu:9002/sparql")
+
+    construct_query = get_construct_query(sg)
+    print("Construct query:\n", construct_query)
+
+    sparql.setQuery(construct_query)
+
+    sparql.setReturnFormat(TURTLE)
+    results = sparql.query().convert()
+    g = Graph()
+    g.parse(data=results, format="turtle")
+    g.serialize(format='turtle')
+
+    filename = lastStringURL(sg)[1]
+    file_path = "data/retrieved/" + filename
+    file = open(file_path, "wb")
+    g.serialize(destination=file, format="turtle")
+    file.flush()
+    file.close()
+
+    return path.abspath(file_path)
+
+
+def main(sg, extended_approach):
+
+    if extended_approach:
+        data_ttl_file = get_reduced_data_from_endpoint(sg)
+    else:
+        # use locally saved data
+        data_ttl_file = "data/hop2.ttl"
+
+    conforms, v_graph, v_text = validate(data_ttl_file, shacl_graph=sg, inference='rdfs',
+                                         serialize_report_graph=True)
+
+    print(v_text)
+
+if __name__ == '__main__':
+    extended_approach = False
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "wrapper":
+            extended_approach = True
+
+    constraints = shapes_graph_to_test()
+
+    for x in constraints:
+        print("\n# File: ", x, "\n")
+
+        sg = x
+        sg = path.abspath(sg)
+
+        start = time.time()
+        main(sg, extended_approach)
+        end = time.time()
+
+        print("Runtime", x, ": ", end - start)
