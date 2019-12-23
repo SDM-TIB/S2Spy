c311723abd5a93fa5e6e74a0a3c9943b4395ef76
monicafiguera <monicafigueray@gmail.com>
2019-12-23 13:12:20 +0100

    conditional to switch between SELECT and CONSTRUCT

--- examples/hop2.py
+++ examples/hop2.py
@@ -2,10 +2,12 @@
 from os import path
 from pyshacl import validate
 from pyshacl import get_query
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON, TURTLE
 import re
 import time
 import argparse
+from rdflib import Graph
+
 
 def get_shapes_graph():
     c1 = "./shapes/smoker.ttl"
@@ -22,6 +24,32 @@ def get_shapes_graph():
 
     return sg
 
+def get_data_turtle_format(query, results):
+    template_for_triples = query
+    pattern = '.*\{'
+    replace = ''
+    string = template_for_triples
+    template_for_triples = re.sub(pattern, replace, string)
+    pattern = '\}.*'
+    replace = ''
+    string = template_for_triples
+    template_for_triples = re.sub(pattern, replace, string)
+    print(template_for_triples)
+
+    data_turtle_format = ''
+    triples = []
+    for result in results["results"]["bindings"]:
+        template_copy = template_for_triples
+        for key, props in result.items():
+            pattern = r"\?" + re.escape(key) + r"\b"
+            print(pattern)
+            if props["type"] == "literal":
+                template_copy = re.sub(pattern, '"' + props["value"] + '"', template_copy)
+            elif props["type"] == "uri":
+                template_copy = re.sub(pattern, ' <' + props["value"] + '> ', template_copy)
+            triples.append(template_copy)
+
+    return data_turtle_format.join(triples)
 
 def main(sg, option):
 
@@ -29,37 +57,22 @@ def main(sg, option):
 
     if extended_approach:
         sparql = SPARQLWrapper("http://node3.research.tib.eu:9003/sparql")
+
         query = get_query(sg, option)
         print("Query:\n", query)
+
         sparql.setQuery(query)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-
-        template_for_triples = query
-        pattern = '.*\{'
-        replace = ''
-        string = template_for_triples
-        template_for_triples = re.sub(pattern, replace, string)
-        pattern = '\}.*'
-        replace = ''
-        string = template_for_triples
-        template_for_triples = re.sub(pattern, replace, string)
-        print(template_for_triples)
-
-        data_turtle_format = ''
-        triples = []
-        for result in results["results"]["bindings"]:
-            template_copy = template_for_triples
-            for key, props in result.items():
-                pattern = "\?" + key
-                if props["type"] == "literal":
-                    template_copy = re.sub(pattern, '"' + props["value"] + '"', template_copy)
-                elif props["type"] == "uri":
-                    template_copy = re.sub(pattern, '<' + props["value"] + '>', template_copy)
-                triples.append(template_copy)
-
-        data_turtle_format = data_turtle_format.join(triples)
-        data_graph = data_turtle_format
+        if option == "select":
+            sparql.setReturnFormat(JSON)
+            results = sparql.query().convert()
+            data_graph = get_data_turtle_format(query, results)
+            #print(data_graph)
+        else:
+            sparql.setReturnFormat(TURTLE)
+            results = sparql.query().convert()
+            g = Graph()
+            g.parse(data=results, format="turtle")
+            data_graph = g
 
         conforms, v_graph, v_text = validate(data_graph, shacl_graph=sg, inference='rdfs',
                                              serialize_report_graph=True, data_graph_format='turtle')
