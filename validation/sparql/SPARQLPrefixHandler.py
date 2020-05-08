b90e6a91ca8a1e0a63708e659691ef0e9eddb11a
Philipp Rohde <Philipp.Rohde@tib.eu>
2020-05-08 15:34:11 +0200

    add LUBM shapes

--- validation/sparql/SPARQLPrefixHandler.py
+++ validation/sparql/SPARQLPrefixHandler.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 prefixes = {
+    "ub": "<http://swat.cse.lehigh.edu/onto/univ-bench.owl#>",
+    "rdf": "<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
     "dbo": "<http://dbpedia.org/ontology/>",
     "dbr": "<http://dbpedia.org/resource/>",
     "yago": "<http://dbpedia.org/class/yago/>",
