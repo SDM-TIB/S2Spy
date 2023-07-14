ef46a8235a2a0ff1c60993850ae4ed40875a7d1f
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-07-14 12:27:31 +0200

    release of v1.3.0

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+VERSION = '1.3.0'
+
 setup(
     name='SHACL2SPARQLpy',
-    version='1.2.0',
+    version=VERSION,
     packages=['SHACL2SPARQLpy', 'SHACL2SPARQLpy.constraints', 'SHACL2SPARQLpy.core', 'SHACL2SPARQLpy.sparql', 'SHACL2SPARQLpy.utils'],
     license='MIT',
     author='Mónica Figuera, Philipp D. Rohde',
     author_email='philipp.rohde@tib.eu',
     url='https://github.com/SDM-TIB/SHACL2SPARQLpy',
-    download_url='https://github.com/SDM-TIB/SHACL2SPARQLpy/archive/refs/tags/v1.1.0.tar.gz',
+    download_url='https://github.com/SDM-TIB/SHACL2SPARQLpy/archive/refs/tags/v' + VERSION + '.tar.gz',
     description='Python reference implementation of SHACL2SPARQL',
     long_description=long_description,
     long_description_content_type="text/markdown",
